56828a6a92cbc625fa438a40eaf824c145dcb0a6
David Domingos <davidfabienebg@gmail.com>
2023-04-04 18:20:05 -0300

    Adicionando estrutura no codigo

--- aprendendoUsarGit.py
+++ aprendendoUsarGit.py
@@ -1,14 +1,18 @@
 if __name__ == '__main__':
     opcao = -1
-    while opcao != 2:
+    while opcao != 3:
         print('Calculadora Git')
         print("=" * 20)
         print('1 - Somar')
-        print('2 - Sair')
+        print('2 - Subtração')
+        print('3 - Sair')
         print("=" * 20)
         opcao = int(input("Opção: "))
 
         if opcao == 1:
              print('opção somar')
         elif opcao == 2:
+             print('Opção Subtração')
+             
+        elif opcao == 3:
              print('Obrigado por usar a nossa calculadora')
